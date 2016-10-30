d527d4b7b8a24f9d14467fdf83e2f5bb21025175
nuffe <erik.cfr@gmail.com>
2016-10-30 13:17:43 +0100

    speed up Gibbs

--- lab2/src/lda.jl
+++ lab2/src/lda.jl
@@ -9,7 +9,7 @@ function ldac2docterm(dictfile::AbstractString, documentfile::AbstractString)
     n_words = length(dictionary)
     n_topics = length(lines)
 
-    # could perhaps be a sparse or ragged matrix instead
+    # should really be a sparse matrix
     document_matrix = zeros(Int64, n_topics, n_words)
 
     for line in lines
@@ -29,8 +29,8 @@ function lda(document_matrix::AbstractArray,
             alpha::Float64, beta::Float64, T::Int,
             n_iter=100, verbose=true, seed=1234)
 
-    # Basic implementation of LDA with Gibbs sampling
-    # Using the notation and description from
+    # Basic implementation of LDA with (collapsed) Gibbs sampling
+    # Following the setup (same notation) from
     # Steyvers, Mark, and Tom Griffiths. "Probabilistic topic models."
     # Handbook of latent semantic analysis 427.7 (2007): 424-440.
 
@@ -44,6 +44,7 @@ function lda(document_matrix::AbstractArray,
     CWT = zeros(Int64, W, T) # n_times word w is assigned to topic j
     CDT = zeros(Int64, D, T) # n_times topic j is assigned to some word token in document d
 
+    # Pre compute the word-doc indices
     doc_word_idx = []
     for doc in 1:D
         push!(doc_word_idx, find(document_matrix[doc,:]))
@@ -60,11 +61,16 @@ function lda(document_matrix::AbstractArray,
         end
     end
 
+    # Pre compute the CWT and CDT sums as well
+    sumCWT = vec(sum(CWT, 1))
+    sumCDT = vec(sum(CDT, 1))
+
     START = time()
     for iter in 1:n_iter
         if verbose
-            if iter % 10 == 0
-                println("iteration: $iter, elapsed time: $(time() - START)s")
+            if iter % 50 == 0
+                println("iteration: $iter, elapsed time:$(
+                @sprintf("%.2f", time() - START))s")
             end
         end
         for doc in 1:D
@@ -74,20 +80,22 @@ function lda(document_matrix::AbstractArray,
                 CWT[word, z] -= 1
                 CDT[doc, z] -= 1
 
-                # Conditional prob
+                # Conditional probs
+                # How much each topic "like" this word (smoothed by `beta`)
                 left = (CWT[word, :] + beta) ./
-                        (vec(sum(CWT, 1)) + W * beta)
+                        (sumCWT + W * beta)
+                # How much this word "like" the current topic (smoothed by `alpha`)
                 right = (CDT[doc, :] + alpha ) ./
-                        (vec(sum(CDT, 1)) + T * alpha)
+                        (sumCDT + T * alpha)
                 pz = left .* right
                 pz /= sum(pz)
 
                 # Sample new topic
                 # z = indmax(rand(Multinomial(1, pz / sum(pz))))
-                # faster
+                # faster:
                 z = searchsortedfirst(cumsum(pz), rand())
 
-                # Update
+                # Update assignments
                 CWT[word, z] += 1
                 CDT[doc, z] += 1
                 TA[doc, i] = z
